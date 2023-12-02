1ae9106c6598fd510f1c075116b85a51dfc14400
aqaacc <mralextb@gmail.com>
2023-12-02 23:16:54 +0100

    Initial commit

---
+++ src/test/java/com/aqa/ReadingFromProperties.java
@@ -0,0 +1,22 @@
+package com.aqa;
+
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadingFromProperties {
+    public static final String filePath = (System.getProperty("user.dir") + "\\src\\file.properties");
+
+    @Test
+    public void firstTest() throws IOException {
+        Properties properties = new Properties();
+        FileInputStream fileInputStream = new FileInputStream(filePath);
+        properties.load(fileInputStream);
+        System.out.println(properties.getProperty("dob"));
+
+
+    }
+}
+
